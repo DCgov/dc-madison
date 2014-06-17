1ae62a3b1edb1a8b2b4467d9b0239ff05276928b
Chris Birk <cmbirk@gmail.com>
2014-06-17 14:08:17 -0400

    update repo

--- app/config/packages/anahkiasen/rocketeer/scm.php
+++ app/config/packages/anahkiasen/rocketeer/scm.php
@@ -8,7 +8,7 @@
 
 	// The SSH/HTTPS address to your repository
 	// Example: https://github.com/vendor/website.git
-	'repository' => 'https://github.com/opengovfoundation/madison.git',
+	'repository' => 'https://github.com/opengovfoundation/dc-madison.git',
 
 	// The repository credentials : you can leave those empty
 	// if you're using SSH or if your repository is public
