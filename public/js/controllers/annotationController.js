12d631def84a53c0bd317eb27d98b7c5dd6b008b
Seth Etter <sethetter@gmail.com>
2016-01-13 12:14:02 -0600

    Put annotation help tooltip back

--- public/js/controllers/annotationController.js
+++ public/js/controllers/annotationController.js
@@ -13,9 +13,9 @@ angular.module('madisonApp.controllers')
       $scope.annotationsShow = false;
       $scope.currentGroup = null;
 
-      //setTimeout(function() {
-      //  $('.annotation-help').tooltip({});
-      //}, 1000);
+      setTimeout(function() {
+        $('.annotation-help').tooltip({});
+      }, 1000);
 
       //Parse sub-comment hash if there is one
       var hash = $location.hash();
